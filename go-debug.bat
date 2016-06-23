33bdbf1e21d8620eedf031fe41c2310813f26e55
Evgeny Himmelreich <evgeny.himmelreich@sap.com>
2016-06-23 17:32:26 +0300

    Style for charts and fixes in headers normalization

--- go-debug.bat
+++ go-debug.bat
@@ -1,2 +1,13 @@
 set MAVEN_OPTS=-Xdebug -Xrunjdwp:transport=dt_socket,server=y,address=8000,suspend=n -Dhttp.proxyHost=proxy -Dhttp.proxyPort=8080 -Dhttps.proxyHost=proxy -Dhttps.proxyPort=8080
-mvn hpi:run -Djetty.port=8090
+
+echo.
+echo ########### CLEAN ###########
+call mvn clean 
+
+echo.
+echo ########### INSTALL ###########
+call mvn install -DskipTests=true 
+
+echo.
+echo ########### HPI:RUN ###########
+call mvn hpi:run -Djetty.port=8090  -DskipTests=true 
--- src/main/java/perflab/CSVAnalyzer/Analysis.java
+++ src/main/java/perflab/CSVAnalyzer/Analysis.java
@@ -1,18 +1,16 @@
 package perflab.CSVAnalyzer;
 
-import java.io.BufferedReader;
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Font;
 import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintStream;
 import java.io.RandomAccessFile;
 import java.util.Calendar;
 import java.util.Collection;
-import java.util.Hashtable;
 import java.util.LinkedList;
-import java.util.TimeZone;
 import java.sql.*;
-import java.time.ZoneId;
 
 import org.relique.jdbc.csv.CsvDriver;
 
@@ -24,18 +22,15 @@ import org.apache.commons.io.FilenameUtils;
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartUtilities;
 import org.jfree.chart.JFreeChart;
-import org.jfree.chart.plot.PlotOrientation;
-import org.jfree.data.category.DefaultCategoryDataset;
+import org.jfree.chart.block.BlockBorder;
+import org.jfree.chart.renderer.xy.XYItemRenderer;
+import org.jfree.chart.title.TextTitle;
 import org.jfree.data.general.SeriesException;
-import org.jfree.data.time.Day;
-import org.jfree.data.time.Millisecond;
-import org.jfree.data.time.RegularTimePeriod;
 import org.jfree.data.time.Second;
 import org.jfree.data.time.TimeSeries;
 import org.jfree.data.time.TimeSeriesCollection;
 import org.jfree.data.time.TimeSeriesDataItem;
-import org.jfree.data.xy.XYDataset;
-import org.jfree.data.xy.XYSeriesCollection;
+import org.jfree.ui.HorizontalAlignment;
 
 public class Analysis {
 	
@@ -67,10 +62,24 @@ public class Analysis {
 		  logger.println("[Analysis] : Executing CSV analysis for folder " + this.csvFiles);
 			
 		  Collection<File> filesToAnalyze = getAllFilesThatMatchFilenameExtension(csvFiles, "csv");
-		  for (File oneFile : filesToAnalyze){
-			  normalizeCSVHeader(oneFile);
-		  }
+		  
+		  LinkedList<File> filesNotToAnalyze = new LinkedList<File>();
 
+		  for (File oneFile : filesToAnalyze){
+			  boolean toKeep = true;
+			  try {
+				  toKeep = normalizeCSVHeader(oneFile);
+			  }catch(Exception e){
+				  toKeep = false;
+			  }
+			  
+			  if(!toKeep){
+				  logger.println("[Analysis] [ERROR] - Can't normalize " + oneFile + " header, removing form the list...");
+				  filesNotToAnalyze.add(oneFile);
+			  }
+		  }
+		  filesToAnalyze.removeAll(filesNotToAnalyze);
+		  
 	      // Load the driver.
 	      Class.forName("org.relique.jdbc.csv.CsvDriver");
 	      Connection conn = DriverManager.getConnection("jdbc:relique:csv:" + this.csvFiles);
@@ -90,61 +99,93 @@ public class Analysis {
 			  }
 		  }
 
-
-	    	  ///////////////////////////////////////////////////////////////////
-	    	  int metricIdx = 0;
-	    	  int numOfMetrics = metrics.size();
-		      for(String metric : metrics) {
-		    	  metricIdx++;
-		    	  logger.println("#####\tExtracting metric (" + metricIdx + "/" + numOfMetrics + ") : " + metric);
+    	  ///////////////////////////////////////////////////////////////////
+    	  int metricIdx = 0;
+    	  int numOfMetrics = metrics.size();
+	      for(String metric : metrics) {
+	    	  metricIdx++;
+	    	  logger.println("#####\tExtracting metric (" + metricIdx + "/" + numOfMetrics + ") : " + metric);
+	    	  
+	    	  TimeSeriesCollection seriesCollection = new TimeSeriesCollection();
+	    	  
+			  //////////////////////////////////////////////////////////////////////
+			  for (File oneFile : filesToAnalyze){
+		    	  //logger.println("[Analysis]:analyzing file: " + oneFile);
 		    	  
-		    	  TimeSeriesCollection seriesCollection = new TimeSeriesCollection();
+		    	  // Select the ID and NAME columns from sample.csv
+		    	  String scvFileNoExtension = FilenameUtils.getBaseName(oneFile.getName());
+			      // Retrieve custom column names
+		    	  LinkedList<KeyObject> uniqueKeys = getUniqueKeys(conn, scvFileNoExtension);
 		    	  
-				  //////////////////////////////////////////////////////////////////////
-				  for (File oneFile : filesToAnalyze){
-			    	  //logger.println("[Analysis]:analyzing file: " + oneFile);
-			    	  
-			    	  // Select the ID and NAME columns from sample.csv
-			    	  String scvFileNoExtension = FilenameUtils.getBaseName(oneFile.getName());
-				      // Retrieve custom column names
-			    	  LinkedList<KeyObject> uniqueKeys = getUniqueKeys(conn, scvFileNoExtension);
-			    	  
-			    	  int keyIdx = 0;
-			    	  int numOfKeys = uniqueKeys.size();
-			    	  
-			    	  try{
-			    		  for(KeyObject key : uniqueKeys){
-				    		  keyIdx++;
-				    		  logger.println("#####\t\t(" + keyIdx + "/" + numOfKeys + ") from " + scvFileNoExtension);
-				    		  TimeSeries tseries = getDataset(conn, scvFileNoExtension, metric, key.getWhereClause(), key);	
-				    		  seriesCollection.addSeries(tseries);
-				    	  } 
-			    	  }catch(SQLException sqlex){
-			    		  logger.println("[ERROR]\t\tCan't extract "+metric+" form  " + scvFileNoExtension + " : " + sqlex.getMessage());
-			    	  }
-				  }/// end of files loop
+		    	  int keyIdx = 0;
+		    	  int numOfKeys = uniqueKeys.size();
 		    	  
-		    	  String imageFile = csvFiles + "\\"+metric+".png";
-		    	  logger.println(String.format("#####\tDrawing [%s] to [%s]", metric, imageFile));
+		    	  try{
+		    		  for(KeyObject key : uniqueKeys){
+			    		  keyIdx++;
+			    		  logger.println("#####\t\t(" + keyIdx + "/" + numOfKeys + ") from " + scvFileNoExtension);
+			    		  TimeSeries tseries = getDataset(conn, scvFileNoExtension, metric, key.getWhereClause(), key);	
+			    		  seriesCollection.addSeries(tseries);
+			    	  } 
+		    	  }catch(SQLException sqlex){
+		    		  logger.println("[ERROR]\t\tCan't extract "+metric+" form  " + scvFileNoExtension + " : " + sqlex.getMessage());
+		    	  }
+			  }/// end of files loop
+	    	  
+	    	  String imageFile = csvFiles + "\\"+metric+".png";
+	    	  logger.println(String.format("#####\tDrawing [%s] to [%s]", metric, imageFile));
 
-			      JFreeChart timechart = ChartFactory.createTimeSeriesChart(
-					         metric,           //title
-					         "Time",           //x label
-					         "Value",          //y label
-					         seriesCollection, //dataset
-					         true,             //legend 
-					         true,             //tooltips
-					         false);            //generate urls ?
+		      JFreeChart timechart = ChartFactory.createTimeSeriesChart(
+				         metric,           //title
+				         "Time",           //x label
+				         "Value",          //y label
+				         seriesCollection, //dataset
+				         true,             //legend 
+				         true,             //tooltips
+				         false);           //generate urls ?
 
-			      int width = 1024;  //Width of the image 
-			      int height = 768;  //Height of the image
-			      File timeChartFile = new File( csvFiles + "\\"+metric+".png" );
-			      
-			      ChartUtilities.saveChartAsPNG(timeChartFile, timechart, width, height);
-			      logger.println("#####################################################################");
+		      int width = 1024;  //Width of the image 
+		      int height = 768;  //Height of the image
+		      File timeChartFile = new File( csvFiles + "\\"+metric+".png" );
+		          
+		      
+		      BasicStroke brokenLine = new BasicStroke(2.2f, //  Line weight
+                      BasicStroke.CAP_ROUND, //  Endpoint style
+                      BasicStroke.JOIN_ROUND, //  Interpretive style
+                      8f, new float[] { 5.0f }, 0.6f);
+		      
+		      BasicStroke solidLine = new BasicStroke(1.8f);
+		      
+		      XYItemRenderer renderer = timechart.getXYPlot().getRenderer();
+		      int seriesCount = timechart.getXYPlot().getSeriesCount();
+		      for (int i = 0; i < seriesCount; i++) {
+		    	  
+                  if (i % 2 == 0){
+                      renderer.setSeriesStroke(i, solidLine); //  Use the solid line drawing
+                  }else{
+                      renderer.setSeriesStroke(i, brokenLine); //  Use a dotted line drawing
+                  }
 		      }
-		  
-	      
+
+		      timechart.setTextAntiAlias(true);
+		      timechart.setBackgroundPaint(new Color(221,236,254));
+		      		      
+		      timechart.setTitle(new TextTitle(metric, new Font("Palatino", Font.BOLD,16)));
+		      timechart.getXYPlot().getDomainAxis().setLabelFont(new Font("Palatino", Font.PLAIN, 14));
+		      timechart.getXYPlot().getRangeAxis().setLabelFont(new Font("Palatino", Font.PLAIN, 14));
+
+		      //XYAnnotation annotation = new XYTextAnnotation("ZZZZZZZZZZZZZZZZ", 500, 500);
+		      //timechart.getXYPlot().addAnnotation(annotation);
+		      
+		      timechart.getLegend().setItemFont(new Font("Palatino", Font.PLAIN, 14));
+		      timechart.getLegend().setFrame(BlockBorder.NONE);
+		      timechart.getLegend().setHorizontalAlignment(HorizontalAlignment.CENTER);
+		      
+		      //timechart.addSubtitle(new TextTitle("(Powered by Perflab)", new Font("Palatino", Font.ITALIC,8)));
+		      
+		      ChartUtilities.saveChartAsPNG(timeChartFile, timechart, width, height);
+		      logger.println("#####################################################################");
+	      }	      
 	      // Clean up
 	      conn.close();
 	    }
@@ -157,20 +198,33 @@ public class Analysis {
 		return okay;
 	}
 	
-	private void normalizeCSVHeader(File csvFile) throws IOException {
-
-		RandomAccessFile raInputFile = new RandomAccessFile(csvFile, "rw");
-		String origHeaderRow = raInputFile.readLine();
+	private boolean normalizeCSVHeader(File csvFile) throws IOException {
+		boolean okay = true;
+		logger.println("[Analysis] Normalizing CSV heafers for " + csvFile);
 		
-		origHeaderRow = origHeaderRow.replaceAll(" ", "_");             //replace all spaces
-		origHeaderRow = origHeaderRow.replaceAll("[^A-Za-z0-9,_]", "_");//replace all non alpha numeric, not comma and not _
-		origHeaderRow = origHeaderRow.replaceFirst(",$", " ");          //remove trailing comma
-		
-		raInputFile.seek(0);
-		raInputFile.writeBytes(origHeaderRow);
-		raInputFile.close();
-		
-		logger.println("Finished to fix file " + csvFile);
+		if(csvFile.exists() && csvFile.isFile()){
+			RandomAccessFile raInputFile = new RandomAccessFile(csvFile, "rw");
+			if(raInputFile.length() > 0 ){
+				String origHeaderRow = raInputFile.readLine();
+				
+				origHeaderRow = origHeaderRow.replaceAll(" ", "_");             //replace all spaces
+				origHeaderRow = origHeaderRow.replaceAll("[^A-Za-z0-9,_]", "_");//replace all non alpha numeric, not comma and not _
+				origHeaderRow = origHeaderRow.replaceFirst(",$", " ");          //remove trailing comma
+				
+				raInputFile.seek(0);
+				raInputFile.writeBytes(origHeaderRow);
+				raInputFile.close();
+			
+				okay = true;
+			}else{
+				logger.println("[Analysis][ERROR] Can't normalize CSV file header, file is empty");
+				okay = false;
+			}
+		}else{
+			logger.println("[Analysis][ERROR] Can't normalize CSV file header");
+			okay = false;
+		}
+		return okay;
 	}
 
 	private TimeSeries getDataset(Connection conn, String table, String metric, String where, KeyObject key) throws SQLException{
@@ -270,7 +324,8 @@ public class Analysis {
 		return customColumns;
 	}
 
-	private Collection getAllFilesThatMatchFilenameExtension(String directoryName, String extension)
+	@SuppressWarnings("unchecked")
+	private Collection<File> getAllFilesThatMatchFilenameExtension(String directoryName, String extension)
 	{
 	  File directory = new File(directoryName);
 	  
@@ -287,7 +342,6 @@ public class Analysis {
         logger.println("</div>");
     }
     
-	
 	class KeyObject{
 		private String hostName;
 		private String processName;
